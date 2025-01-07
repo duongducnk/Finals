ea7e003ffa503de0b73c0049269e2d10bd4a537e
duongducnk <58649643+duongducnk@users.noreply.github.com>
2025-01-07 02:29:15 +0000

    fix_deploy

--- TravelAdvisor/tailwind.config.js
+++ TravelAdvisor/tailwind.config.js
@@ -1,4 +1,7 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
     darkMode: ["class"],
     content: [
@@ -56,5 +59,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
